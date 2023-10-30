72960e8184def1c7702ee591074345345f0f5cf5
binary-blazer <janjytapyt@sdevs.org>
2023-10-30 11:01:31 +0100

    Update search.ts

--- src/functions/search.ts
+++ src/functions/search.ts
@@ -6,10 +6,18 @@ import { JSONParser } from "../util/jsonParser";
 import { noop } from "../util/noop";
 import { Util } from "../util/Util";
 
-export async function search(query: string) {
+export async function search(query: string, amount: number = 1): Promise<YoutubeSearchResults> {
     const params = new URLSearchParams()
     
     params.append("search_query", query)
+    params.append("sp", "EgIQAQ%3D%3D")
+    params.append("pbj", "1")
+
+    if (amount > 1) {
+        params.append("pbjreload", "1")
+    } else {
+        params.append("pbjreload", `${amount}`)
+    }
 
     params.append("hl", "en")
 
